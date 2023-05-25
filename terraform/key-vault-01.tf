9eb54b6487c9c078e0bf6d60574fdcff8f97eb66
Fraser Molyneux <fmolyneux@microsoft.com>
2023-05-25 15:12:27 +0100

    Adding secret

--- terraform/key-vault-01.tf
+++ terraform/key-vault-01.tf
@@ -17,8 +17,35 @@ resource "azurerm_key_vault" "kv_01" {
 
   soft_delete_retention_days = 7
 
+  enable_rbac_authorization   = true
   enabled_for_disk_encryption = true
   purge_protection_enabled    = true
 
   sku_name = "standard"
+}
+
+resource "azurerm_role_assignment" "kv_01_sp" {
+  for_each = toset(var.locations)
+
+  scope                = azurerm_key_vault.kv_01[each.value].id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = data.azurerm_client_config.current.object_id
+}
+
+resource "azurerm_role_assignment" "kv_01_demo" {
+  for_each = toset(var.locations)
+
+  scope                = azurerm_key_vault.kv_01[each.value].id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = "3270dd31-29ac-486d-8a16-e9179660c8d7"
+}
+
+resource "azurerm_key_vault_secret" "kv_01_example" {
+  name         = "my-super-secret"
+  value        = random_string.location[each.value].result
+  key_vault_id = azurerm_key_vault.kv_01[each.value].id
+
+  depends_on = [
+    azurerm_role_assignment.kv_01_sp
+  ]
 }
