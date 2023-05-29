c41a7db8a1e46c424637d525961f6cc5a6154aac
Fraser Molyneux <frasermolyneux@mx-mail.io>
2023-05-29 07:53:40 +0100

    Remove sp role assignments

--- terraform/key-vault-01-role-assignments.tf
+++ terraform/key-vault-01-role-assignments.tf
@@ -1,11 +1,3 @@
-resource "azurerm_role_assignment" "kv_01_sp" {
-  for_each = toset(var.locations)
-
-  scope                = azurerm_key_vault.kv_01[each.value].id
-  role_definition_name = "Key Vault Secrets Officer"
-  principal_id         = data.azurerm_client_config.current.object_id
-}
-
 resource "azurerm_role_assignment" "kv_01_demo" {
   for_each = toset(var.locations)
 
--- terraform/key-vault-02-role-assignments.tf
+++ terraform/key-vault-02-role-assignments.tf
@@ -1,11 +1,3 @@
-resource "azurerm_role_assignment" "kv_02_sp" {
-  for_each = toset(var.locations)
-
-  scope                = azurerm_key_vault.kv_02[each.value].id
-  role_definition_name = "Key Vault Secrets Officer"
-  principal_id         = data.azurerm_client_config.current.object_id
-}
-
 resource "azurerm_role_assignment" "kv_02_demo" {
   for_each = toset(var.locations)
 
