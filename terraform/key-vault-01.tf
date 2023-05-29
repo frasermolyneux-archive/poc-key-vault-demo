73248ac915d30441caaf9b74396e85b454d76277
Fraser Molyneux <frasermolyneux@mx-mail.io>
2023-05-29 07:37:49 +0100

    Reworking permissions

--- terraform/key-vault-01.tf
+++ terraform/key-vault-01.tf
@@ -22,27 +22,3 @@ resource "azurerm_key_vault" "kv_01" {
 
   sku_name = "standard"
 }
-
-resource "azurerm_role_assignment" "kv_01_sp" {
-  for_each = toset(var.locations)
-
-  scope                = azurerm_key_vault.kv_01[each.value].id
-  role_definition_name = "Key Vault Secrets Officer"
-  principal_id         = data.azurerm_client_config.current.object_id
-}
-
-resource "azurerm_role_assignment" "kv_01_demo" {
-  for_each = toset(var.locations)
-
-  scope                = azurerm_key_vault.kv_01[each.value].id
-  role_definition_name = "Key Vault Secrets User"
-  principal_id         = "3270dd31-29ac-486d-8a16-e9179660c8d7"
-}
-
-resource "azurerm_role_assignment" "kv_01_demo_io" {
-  for_each = toset(var.locations)
-
-  scope                = azurerm_key_vault.kv_01[each.value].id
-  role_definition_name = "Key Vault Secrets User"
-  principal_id         = "3270dd31-29ac-486d-8a16-e9179660c8d7"
-}
--- terraform/key-vault-02.tf
+++ terraform/key-vault-02.tf
@@ -29,30 +29,6 @@ resource "azurerm_key_vault" "kv_02" {
   }
 }
 
-resource "azurerm_role_assignment" "kv_02_sp" {
-  for_each = toset(var.locations)
-
-  scope                = azurerm_key_vault.kv_02[each.value].id
-  role_definition_name = "Key Vault Secrets Officer"
-  principal_id         = data.azurerm_client_config.current.object_id
-}
-
-resource "azurerm_role_assignment" "kv_02_demo" {
-  for_each = toset(var.locations)
-
-  scope                = azurerm_key_vault.kv_02[each.value].id
-  role_definition_name = "Key Vault Secrets User"
-  principal_id         = "3270dd31-29ac-486d-8a16-e9179660c8d7"
-}
-
-resource "azurerm_role_assignment" "kv_02_demo_io" {
-  for_each = toset(var.locations)
-
-  scope                = azurerm_key_vault.kv_02[each.value].id
-  role_definition_name = "Key Vault Secrets User"
-  principal_id         = "3270dd31-29ac-486d-8a16-e9179660c8d7"
-}
-
 resource "azurerm_private_endpoint" "kv_02" {
   for_each = toset(var.locations)
 
