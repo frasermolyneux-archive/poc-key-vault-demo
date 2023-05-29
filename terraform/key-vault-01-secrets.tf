4c045e425f245b2fe622b5fbc6ac374636c7f4b4
Fraser Molyneux <frasermolyneux@mx-mail.io>
2023-05-29 07:56:44 +0100

    Remove depends on

--- terraform/key-vault-01-secrets.tf
+++ terraform/key-vault-01-secrets.tf
@@ -4,8 +4,4 @@ resource "azurerm_key_vault_secret" "kv_01_example" {
   name         = "my-super-secret"
   value        = random_string.location[each.value].result
   key_vault_id = azurerm_key_vault.kv_01[each.value].id
-
-  depends_on = [
-    azurerm_role_assignment.kv_01_sp
-  ]
 }
