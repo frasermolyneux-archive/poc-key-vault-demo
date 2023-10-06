93f3e93a246d2b6817123334468ff9b151ca2267
Fraser Molyneux <fmolyneux@microsoft.com>
2023-10-06 10:28:55 +0100

    Remove group example

--- terraform/pim-direct-subscription.tf
+++ terraform/pim-direct-subscription.tf
@@ -4,7 +4,7 @@ resource "time_static" "subscription_contributor" {}
 resource "azurerm_pim_eligible_role_assignment" "subscription_contributor" {
   scope              = data.azurerm_subscription.primary.id
   role_definition_id = "${data.azurerm_subscription.primary.id}${data.azurerm_role_definition.contributor.id}"
-  principal_id       = "3270dd31-29ac-486d-8a16-e9179660c8d7"
+  principal_id       = "3270dd31-29ac-486d-8a16-e9179660c8d7" // For demo purposes - this is fmolyneux@microsoft.com
 
   schedule {
     start_date_time = time_static.subscription_contributor.rfc3339
