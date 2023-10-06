ac4fc103185ae1d8dd12e4a8f7d4a9505e444c69
Fraser Molyneux <fmolyneux@microsoft.com>
2023-10-06 09:44:06 +0100

    Fix definition

--- terraform/pim-bygroup.tf
+++ terraform/pim-bygroup.tf
@@ -22,7 +22,7 @@ resource "azurerm_pim_eligible_role_assignment" "kvofficer_eligible_group_kv01" 
   for_each = toset(var.locations)
 
   scope              = azurerm_key_vault.kv_01[each.value].id
-  role_definition_id = "${data.azurerm_subscription.primary.id}${data.azurerm_role_definition.contributor.id}"
+  role_definition_id = "${data.azurerm_subscription.primary.id}${data.azurerm_role_definition.kvsecretsofficer.id}"
   principal_id       = azuread_group.kvofficer_eligible_group.object_id
 
   schedule {
@@ -36,7 +36,7 @@ resource "azurerm_pim_eligible_role_assignment" "kvofficer_eligible_group_kv02" 
   for_each = toset(var.locations)
 
   scope              = azurerm_key_vault.kv_02[each.value].id
-  role_definition_id = "${data.azurerm_subscription.primary.id}${data.azurerm_role_definition.contributor.id}"
+  role_definition_id = "${data.azurerm_subscription.primary.id}${data.azurerm_role_definition.kvsecretsofficer.id}"
   principal_id       = azuread_group.kvofficer_eligible_group.object_id
 
   schedule {
